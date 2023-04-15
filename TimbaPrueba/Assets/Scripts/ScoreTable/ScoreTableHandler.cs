9cdace5b0e2d466e2818f597e581b2b2a759f727
mhoyos <myshash101@hotmail.com>
2023-04-15 15:30:23 -0500

    Main menu developed, it now loads the leaderboard and generates each element that the API retrieves as a score element. There is an AudioManager script for managing SFX Post process volume added for a clasic 8Bit effect with focal distortion, bloom and vignette Imported SFX assets and soundtrack assets Game scene created for the gameplay (Is empty for now) MainMenuManager added to handle buttons interaction for the main menu

--- TimbaPrueba/Assets/Scripts/ScoreTable/ScoreTableHandler.cs
+++ TimbaPrueba/Assets/Scripts/ScoreTable/ScoreTableHandler.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,16 +10,17 @@ using UnityEngine;
 public class ScoreTableHandler : MonoBehaviour
 {
     [Header("API Setup")]
-    [SerializeField]
+    [SerializeField] private RequestDataSO getScoresRequest;
     private APIRequestHandler APIHandler;
-    [SerializeField]
-    private RequestDataSO getScoresRequest;
 
     [Header("UI Setup")]
-    [SerializeField]
-    private GameObject userElementReference; //Element for factory pattern
-    [SerializeField]
-    private GameObject loadingPanel;
+    [SerializeField] private TableElement userElementReference; //Element for factory pattern
+    [SerializeField] private Transform scrollViewContentTransform;
+    [SerializeField] private GameObject loadingPanel;
+
+    private List<GameObject> generatedScores = new List<GameObject>(); //Lista de elementos actuales generados en la tabla
+
+    private void Start() => APIHandler = APIRequestHandler.Instance;
 
     /// <summary>
     /// Inicializa la tabla solicitando los datos de puntuación
@@ -28,13 +30,51 @@ public class ScoreTableHandler : MonoBehaviour
         loadingPanel.SetActive(true);
 
         string response = await APIHandler.MakeHTTPRequest(getScoresRequest);
-        List<UserScoreSerialized> result = JsonConvert.DeserializeObject<List<UserScoreSerialized>>(response);
-
-        GenerateTable(result);
+        if (!string.IsNullOrEmpty(response))
+        {
+            List<UserScoreSerialized> result = JsonConvert.DeserializeObject<List<UserScoreSerialized>>(response);
+            GenerateTable(result);
+        }
+        else
+        {
+            Debug.LogWarning("Table couldn't be created, didn't get a response from Database");
+            loadingPanel.SetActive(false);
+        }
     }
 
     private void GenerateTable(List<UserScoreSerialized> scores)
     {
+        //Limpiar la tabla
+        if(generatedScores.Count > 0)
+        {
+            foreach(GameObject listElement in generatedScores)
+            {
+                Destroy(listElement);
+            }
+            generatedScores = new List<GameObject>();
+        }
 
+        //Generar la tabla con los usuarios con factory pattern
+        try
+        {
+            int scoreIndex = 1;
+            foreach (UserScoreSerialized score in scores)
+            {
+                if(!string.IsNullOrEmpty(score.usuario))
+                {
+                    TableElement newElement = Instantiate(userElementReference.gameObject, scrollViewContentTransform).GetComponent<TableElement>();
+                    newElement.SetTableElementData(scoreIndex.ToString(), score.usuario, score.puntaje.ToString());
+                    generatedScores.Add(newElement.gameObject);
+                    newElement.gameObject.SetActive(true);
+                    scoreIndex++;
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            Debug.LogError("Error creating table :: " + e.Message);
+        }
+
+        loadingPanel.SetActive(false);
     }
 }
